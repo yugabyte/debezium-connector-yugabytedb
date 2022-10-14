84b50f4cf31c5514d091bc86cf394a91f0d701da
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2022-10-14 09:48:09 +0000

    changes to handle multiple partitions per task

--- src/main/java/io/debezium/connector/yugabytedb/YBPartition.java
+++ src/main/java/io/debezium/connector/yugabytedb/YBPartition.java
@@ -1,8 +1,17 @@
 package io.debezium.connector.yugabytedb;
 
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 
+import org.apache.commons.lang3.tuple.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.debezium.DebeziumException;
 import io.debezium.pipeline.spi.Partition;
 import io.debezium.util.Collect;
 
@@ -50,5 +59,34 @@ public class YBPartition implements Partition {
         return "YBPartition{" +
                 "tabletId='" + tabletId + '\'' +
                 '}';
+    }
+
+    static class Provider implements Partition.Provider<YBPartition> {
+        private final YugabyteDBConnectorConfig connectorConfig;
+        private static final Logger LOGGER = LoggerFactory.getLogger(YBPartition.class);
+
+        Provider(YugabyteDBConnectorConfig connectorConfig) {
+            this.connectorConfig = connectorConfig;
+        }
+
+        @Override
+        public Set<YBPartition> getPartitions() {
+            String tabletList = this.connectorConfig.getConfig().getString(YugabyteDBConnectorConfig.TABLET_LIST);
+            List<Pair<String, String>> tabletPairList = null;
+            try {
+                tabletPairList = (List<Pair<String, String>>) ObjectUtil.deserializeObjectFromString(tabletList);
+                LOGGER.debug("The tablet list is " + tabletPairList);
+            } catch (IOException | ClassNotFoundException e) {
+                // The task should fail if tablet list cannot be deserialized
+                throw new DebeziumException("Error while deserializing tablet list", e);
+            }
+
+            Set<YBPartition> partititons = new HashSet<>();
+            for (Pair<String, String> tabletPair : tabletPairList) {
+                partititons.add(new YBPartition(tabletPair.getRight()));
+            }
+            LOGGER.debug("The partition being returned is " + partititons);
+            return partititons;
+        }
     }
 }
