eb97bcd8fea10e84c183a94ae9628649d4473b2d
Rajat Venkatesh <1638298+vrajat@users.noreply.github.com>
2022-10-17 13:45:05 +0530

    Implement stream safe time and peek

--- src/main/java/io/debezium/connector/yugabytedb/consistent/Merger.java
+++ src/main/java/io/debezium/connector/yugabytedb/consistent/Merger.java
@@ -10,22 +10,40 @@ public class Merger {
     private static final Logger LOGGER = LoggerFactory.getLogger(Merger.class);
     private final PriorityQueue<Message> queue = new PriorityQueue<>();
     private final Map<String, List<Message>> mergeSlots = new HashMap<>();
+    private final Map<String, Long> tabletSafeTime = new HashMap<>();
 
     public Merger(List<String> tabletList) {
-        tabletList.forEach(tabletId -> mergeSlots.put(tabletId, new ArrayList<>()));
+        tabletList.forEach(tabletId -> {
+            mergeSlots.put(tabletId, new ArrayList<>());
+            tabletSafeTime.put(tabletId, 0L);
+        });
     }
 
     public void addMessage(Message message) {
         if (message.record.getRowMessage().getOp() == CdcService.RowMessage.Op.SAFEPOINT) {
-            LOGGER.info("Received safe point message {}", message);
+            LOGGER.debug("Received safe point message {}", message);
+            tabletSafeTime.put(message.tablet, message.commitTime);
+            LOGGER.debug("Put {}:{}, verifying {}", message.tablet, message.commitTime, tabletSafeTime.get(message.tablet));
+            return;
         }
         queue.add(message);
         mergeSlots.get(message.tablet).add(message);
         LOGGER.info("Add message {}", message);
     }
 
-    public Message peek() {
-        return queue.peek();
+    public Long streamSafeTime() {
+        // tabletSafeTime.entrySet().stream().forEach(e -> LOGGER.info("Tablet {}, safe time {}", e.getKey(), e.getValue()));
+        return Collections.min(tabletSafeTime.values());
+    }
+
+    public Optional<Message> peek() {
+        Message message = queue.peek();
+        Optional<Message> peeked = message != null && message.commitTime < this.streamSafeTime()
+                ? Optional.of(message) : Optional.empty();
+
+        LOGGER.info("Stream Safe Time {}, Top message is {} ", this.streamSafeTime(), peeked);
+
+        return peeked;
     }
 
     public Message poll() {
