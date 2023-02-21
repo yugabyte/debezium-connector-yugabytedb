a29136682968f658051c853f2b7fb9f062955cc6
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2023-02-21 17:09:11 +0530

    uncomment code

--- src/test/java/io/debezium/connector/yugabytedb/consistent/YugabyteDBStreamConsistencyTest.java
+++ src/test/java/io/debezium/connector/yugabytedb/consistent/YugabyteDBStreamConsistencyTest.java
@@ -48,11 +48,11 @@ public class YugabyteDBStreamConsistencyTest extends YugabyteDBTestBase {
     private static final String SETUP_TABLES_STMT = CREATE_TABLES_STMT + INSERT_STMT;
     @BeforeClass
     public static void beforeClass() throws SQLException {
-//        ybContainer = TestHelper.getYbContainer(null, "cdc_max_stream_intent_records=10,cdc_populate_safepoint_record=true");
-//        ybContainer.start();
-//
-//        TestHelper.setContainerHostPort(ybContainer.getHost(), ybContainer.getMappedPort(5433));
-//        TestHelper.setMasterAddress(ybContainer.getHost() + ":" + ybContainer.getMappedPort(7100));
+        ybContainer = TestHelper.getYbContainer(null, "cdc_max_stream_intent_records=10,cdc_populate_safepoint_record=true");
+        ybContainer.start();
+
+        TestHelper.setContainerHostPort(ybContainer.getHost(), ybContainer.getMappedPort(5433));
+        TestHelper.setMasterAddress(ybContainer.getHost() + ":" + ybContainer.getMappedPort(7100));
         TestHelper.dropAllSchemas();
     }
 
