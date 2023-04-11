922cab54bdbdecd02c8b05be5dd67170e4a17cea
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2023-04-11 12:39:50 +0530

    addded tests to verify transaction metadata

--- src/test/java/io/debezium/connector/yugabytedb/common/TestBaseClass.java
+++ src/test/java/io/debezium/connector/yugabytedb/common/TestBaseClass.java
@@ -2,14 +2,21 @@ package io.debezium.connector.yugabytedb.common;
 
 import io.debezium.connector.yugabytedb.rules.YugabyteDBLogTestName;
 import io.debezium.embedded.AbstractConnectorTest;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.source.SourceRecord;
 import org.awaitility.Awaitility;
 
+import org.fest.assertions.Assertions;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.YugabyteYSQLContainer;
 
 import java.time.Duration;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Base class to have common methods and attributes for the containers to be run.
@@ -32,4 +39,43 @@ public class TestBaseClass extends AbstractConnectorTest {
                     return engine.isRunning();
                 });
     }
+
+  @Override
+  protected String assertBeginTransaction(SourceRecord record) {
+    final Struct begin = (Struct) record.value();
+    final Struct beginKey = (Struct) record.key();
+
+    assertEquals("BEGIN", begin.getString("status"));
+    assertNull(begin.getInt64("event_count"));
+
+    final String txId = begin.getString("id");
+    assertEquals(txId, beginKey.getString("id"));
+
+    assertNotNull(begin.getString("partition_id"));
+
+    return txId;
+  }
+
+  /**
+   * Assert that the passed {@link SourceRecord} is a record for END transaction
+   * @param record the record to assert
+   * @param expectedTxId expected transaction ID this record should have
+   * @param expectedEventCount expected event count in the transaction
+   * @param partitionId the partition to which the record belongs, pass null if this assertion needs
+   *                    to be skipped
+   */
+  protected void assertEndTransaction(SourceRecord record, String expectedTxId, long expectedEventCount, String partitionId) {
+    final Struct end = (Struct) record.value();
+    final Struct endKey = (Struct) record.key();
+
+    assertEquals("END", end.getString("status"));
+    assertEquals(expectedTxId, end.getString("id"));
+    assertEquals(expectedEventCount, end.getInt64("event_count"));
+
+    if (partitionId != null) {
+      assertNotNull(end.getString("partition_id"));
+    }
+
+    assertEquals(expectedTxId, endKey.getString("id"));
+  }
 }
