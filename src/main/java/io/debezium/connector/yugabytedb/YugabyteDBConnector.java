5f8764e4f82a4fd38e523b55ade495bd99da2435
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2023-07-19 09:51:01 +0000

    Fix explicit checkpointing check with transaction ordering

--- src/main/java/io/debezium/connector/yugabytedb/YugabyteDBConnector.java
+++ src/main/java/io/debezium/connector/yugabytedb/YugabyteDBConnector.java
@@ -140,8 +140,7 @@ public class YugabyteDBConnector extends RelationalBaseSourceConnector {
             throw new DebeziumException(e);
         }
 
-        if (this.yugabyteDBConnectorConfig.consistencyMode() != YugabyteDBConnectorConfig.ConsistencyMode.DEFAULT
-              && !enableExplicitCheckpointing) {
+        if (this.yugabyteDBConnectorConfig.transactionOrdering() && !enableExplicitCheckpointing) {
             final String errorMessage = "Explicit checkpointing not enabled in consistent streaming mode, "
                 + "create a stream with explicit checkpointing and try again";
             throw new DebeziumException(errorMessage);
