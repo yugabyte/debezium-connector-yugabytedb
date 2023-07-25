d66ab323e18454c161434ec59cadd626a16e8a70
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2023-07-25 04:16:36 +0000

    removed extra spaces

--- src/main/java/io/debezium/connector/yugabytedb/YugabyteDBStreamingChangeEventSource.java
+++ src/main/java/io/debezium/connector/yugabytedb/YugabyteDBStreamingChangeEventSource.java
@@ -927,13 +927,13 @@ public class YugabyteDBStreamingChangeEventSource implements
             // is not possible on colocated tables, it is safe to assume that the tablets here
             // would be all non-colocated.
             YBPartition p = new YBPartition(tableId, tabletId, false /* colocated */);
-            
+
             // Get the checkpoint for child tablet and unset its time.
             OpId checkpoint = OpId.from(pair.getCdcSdkCheckpoint());
             checkpoint.unsetTime();
-            
+
             offsetContext.initSourceInfo(p, this.connectorConfig, checkpoint);
-            
+
             tabletToExplicitCheckpoint.put(p.getId(), checkpoint.toCdcSdkCheckpoint());
 
             LOGGER.info("Initialized offset context for tablet {} with OpId {}", tabletId, checkpoint);
