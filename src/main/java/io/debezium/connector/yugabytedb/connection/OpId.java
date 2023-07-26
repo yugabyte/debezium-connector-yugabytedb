2ca1c622f1f8007bea6ded37345e1d35fcd69e6a
Vaibhav Kushwaha <34186745+vaibhav-yb@users.noreply.github.com>
2023-07-26 12:21:12 +0530

    Unset time from children tablets upon initialization (#253)

--- src/main/java/io/debezium/connector/yugabytedb/connection/OpId.java
+++ src/main/java/io/debezium/connector/yugabytedb/connection/OpId.java
@@ -45,6 +45,10 @@ public class OpId implements Comparable<OpId> {
         return time;
     }
 
+    public void unsetTime() {
+        this.time = 0;
+    }
+
     public static OpId valueOf(String stringId) {
         if (stringId != null && !stringId.isEmpty()) {
             String[] arr = stringId.split(":");
