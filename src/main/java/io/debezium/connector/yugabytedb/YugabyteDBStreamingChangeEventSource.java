de3a7491f7be4a430cb96b2362d181c378f6fd09
Dhruv Rawat <dhruv.rawat@yugabyte.com>
2023-09-11 17:42:35 +0530

    Add check for received OpID always less than older checkpoint

--- src/main/java/io/debezium/connector/yugabytedb/YugabyteDBStreamingChangeEventSource.java
+++ src/main/java/io/debezium/connector/yugabytedb/YugabyteDBStreamingChangeEventSource.java
@@ -868,6 +868,14 @@ public class YugabyteDBStreamingChangeEventSource implements
 
                     // Parse the string to get the OpId object.
                     OpId tempOpId = OpId.valueOf((String) entry.getValue());
+
+                    if(this.tabletToExplicitCheckpoint.get(entry.getKey()) != null &&
+                            tempOpId.getIndex() < this.tabletToExplicitCheckpoint.get(entry.getKey()).getIndex()) {
+                        LOGGER.warn("The received OpId {} is less than the older checkpoint {} for tablet {}",
+                                    tempOpId.getIndex(), this.tabletToExplicitCheckpoint.get(entry.getKey()).getIndex(), entry.getKey());
+                        continue;
+                    }
+
                     this.tabletToExplicitCheckpoint.put(entry.getKey(), tempOpId.toCdcSdkCheckpoint());
 
                     LOGGER.debug("Committed checkpoint on server for stream ID {} tablet {} with term {} index {}",
