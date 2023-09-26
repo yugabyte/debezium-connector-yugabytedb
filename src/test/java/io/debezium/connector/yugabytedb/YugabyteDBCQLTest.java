fe37bb0ae16e67c59d7a1844b04e7b38502cba2b
Sumukh-Phalgaonkar <sumukhphalgaonkar@gmail.com>
2023-09-26 16:06:40 +0530

    Added a test for before Image with cql

--- src/test/java/io/debezium/connector/yugabytedb/YugabyteDBCQLTest.java
+++ src/test/java/io/debezium/connector/yugabytedb/YugabyteDBCQLTest.java
@@ -78,9 +78,83 @@ public class YugabyteDBCQLTest extends YugabytedTestBase/*YugabyteDBContainerTes
 
     }
 
+    @Test
+    public void testBeforeImageWithCQL() throws Exception {
+        String createKeyspace = "CREATE KEYSPACE IF NOT EXISTS cdctest;";
+        session.execute(createKeyspace);
+
+        session.execute("create table if not exists cdctest.t1(id INT PRIMARY KEY, first_name TEXT, last_name VARCHAR, hours int);");
+
+        String dbStreamId = TestHelper.getNewDbStreamId("cdctest", "t1", true, true, BeforeImageMode.ALL, true);
+        Configuration.Builder configBuilder = TestHelper.getConfigBuilderForCQL("cdctest", "cdctest.t1", dbStreamId);
+        
+        startEngine(configBuilder);
+
+        awaitUntilConnectorIsReady();
+
+        session.execute("INSERT INTO cdctest.t1(id,first_name,last_name,hours) values(1,'Vaibhav', 'Kushwaha', 40);");
+        session.execute("UPDATE cdctest.t1 SET first_name='VKVK', hours=30 WHERE id = 1;");
+
+        // Consume the records and verify that the records should have the relevant information.
+        List<SourceRecord> records = new ArrayList<>();
+        CompletableFuture.runAsync(() -> getRecords(records, 2, 20000)).get();
+
+        // The first record is an insert record with before image as null.
+        SourceRecord insertRecord = records.get(0);
+        assertValueField(insertRecord, "before", null);
+        assertAfterImage(insertRecord, 1, "Vaibhav", "Kushwaha", 40);
+
+        // The second record will be an update record having a before image.
+        SourceRecord updateRecord = records.get(1);
+        assertBeforeImage(updateRecord, 1, "Vaibhav", "Kushwaha", 40);
+        assertAfterImage(updateRecord, 1, "VKVK", "Kushwaha", 30);
+    }
+
     private void verifyRecordCount(long recordsCount) {
         waitAndFailIfCannotConsume(new ArrayList<>(), recordsCount);
     }
 
+    private void assertAfterImage(SourceRecord record, Integer id, String firstName, String lastName,
+            int hours) {
+        assertValueField(record, "after/id/value", id);
+        assertValueField(record, "after/first_name/value", firstName);
+        assertValueField(record, "after/last_name/value", lastName);
+        assertValueField(record, "after/hours/value", hours);
+    }
+
+    private void assertBeforeImage(SourceRecord record, Integer id, String firstName, String lastName,
+            int hours) {
+        assertValueField(record, "before/id/value", id);
+        assertValueField(record, "before/first_name/value", firstName);
+        assertValueField(record, "before/last_name/value", lastName);
+        assertValueField(record, "before/hours/value", hours);
+    }
+
+    private void getRecords(List<SourceRecord> records, long totalRecordsToConsume,
+            long milliSecondsToWait) {
+        AtomicLong totalConsumedRecords = new AtomicLong();
+        long seconds = milliSecondsToWait / 1000;
+        try {
+            Awaitility.await()
+                    .atMost(Duration.ofSeconds(seconds))
+                    .until(() -> {
+                        int consumed = consumeAvailableRecords(record -> {
+                            LOGGER.debug("The record being consumed is " + record);
+                            records.add(record);
+                        });
+                        if (consumed > 0) {
+                            totalConsumedRecords.addAndGet(consumed);
+                            LOGGER.debug("Consumed " + totalConsumedRecords + " records");
+                        }
+
+                        return totalConsumedRecords.get() == totalRecordsToConsume;
+                    });
+        } catch (ConditionTimeoutException exception) {
+            fail("Failed to consume " + totalRecordsToConsume + " records in " + seconds + " seconds",
+                    exception);
+        }
+
+        assertEquals(totalRecordsToConsume, totalConsumedRecords.get());
+    }
 
 }
