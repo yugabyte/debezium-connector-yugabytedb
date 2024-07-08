15762defd347b8d44b95acb5d11eada0bd6cd70b
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2024-07-08 16:03:47 +0530

    removed extra changes

--- src/main/java/io/debezium/connector/yugabytedb/YugabyteDBConnectorTask.java
+++ src/main/java/io/debezium/connector/yugabytedb/YugabyteDBConnectorTask.java
@@ -14,6 +14,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 import io.debezium.heartbeat.HeartbeatFactory;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.kafka.connect.errors.ConnectException;
 import org.apache.kafka.connect.errors.RetriableException;
 import org.apache.kafka.connect.source.SourceRecord;
